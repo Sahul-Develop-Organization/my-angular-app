632b025c6ef74a4b5599fbca6cb996a8699fd590
Organization <57387546+SahulHameed-developer@users.noreply.github.com>
2025-11-19 09:38:06 +0530

    Update CORS headers to allow all origins

--- server/server.js
+++ server/server.js
@@ -11,10 +11,8 @@ app.use(cors());
 app.use(bodyParser.json());
 // handling CORS
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", 
-               "http://localhost:4200");
-    res.header("Access-Control-Allow-Headers", 
-               "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
